811bd68dce99e8200448513328f0f54c43448e3b
Aaron K <kearlyaa@gmail.com>
2016-04-25 23:53:35 +0000

    Add comments and associated tests

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -5,5 +5,6 @@ class Gram < ActiveRecord::Base
   validates :picture, :presence => true  
 
   belongs_to :user
+  has_many :comments
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Grammable::Application.routes.draw do
   devise_for :users
   #devise_for :users # found in example but no explanation
   root "grams#index"
-  resources :grams
+  resources :grams do
+    resources :comments, only: :create
+  end
 end
