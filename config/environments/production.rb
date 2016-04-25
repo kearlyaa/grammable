85dd3d4cf3cf95b7e3f8ba440a858b01eb5452ef
Aaron K <kearlyaa@gmail.com>
2016-04-25 16:23:56 +0000

    Install devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Grammable::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'grammable-aaron-k.herokuapp.com', port: 3030 }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
